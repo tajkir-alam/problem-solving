c56bb257fdce9bf3fb47413f07fcb5d3366f56f6
tajkir-alam <tajkir.alam@gmail.com>
2023-07-27 17:17:27 +0600

    task 6 completed

--- index.js
+++ index.js
@@ -124,6 +124,49 @@ function generateRandomPassword(length) {
     return password;
 }
 
-const passwordLength = 12;
-const randomPassword = generateRandomPassword(passwordLength);
-console.log(randomPassword);
+// const passwordLength = 12;
+// const randomPassword = generateRandomPassword(passwordLength);
+// console.log(randomPassword);
+
+
+// ---------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+// Task 7: Implement a function that converts a Roman numeral to an integer. The function should take a Roman numeral string (e.g., "IX" or "XXI") as input and return the corresponding integer value.
+
+function romanToInteger(romanNumeral) {
+    const romanValues = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    };
+
+    let result = 0;
+    let prevValue = 0;
+
+    for (let i = romanNumeral.length - 1; i >= 0; i--) {
+        const currentChar = romanNumeral[i];
+        const currentValue = romanValues[currentChar];
+
+        if (currentValue < prevValue) {
+            result -= currentValue;
+        } else {
+            result += currentValue;
+        }
+
+        prevValue = currentValue;
+    }
+
+    return result;
+}
+
+// console.log(romanToInteger("IX"));
+// console.log(romanToInteger("XXI"));
+// console.log(romanToInteger("IV"));
+// console.log(romanToInteger("XLII"));
+// console.log(romanToInteger("CMXCIX"));
+

8d4ca8c5dd91c5dad45fc35826ae3f6a93ff9af9
tajkir-alam <tajkir.alam@gmail.com>
2023-07-27 17:06:01 +0600

    task 3 done

--- index.js
+++ index.js
@@ -38,3 +38,37 @@ function positiveSum(array) {
 
 
 
+// Task 3: Write a JavaScript program to find the most frequent element in an array and return it. 
+
+// Example Input: [3, 5, 2, 5, 3, 3, 1, 4, 5] Example Output: 3
+
+
+function findMostFrequentElement(array) {
+    // Create an object to store the frequency of each element
+    const frequencyMap = {};
+
+    // Loop through the array to count the frequency of each element
+    array.forEach(element => {
+        if (frequencyMap[element]) {
+            frequencyMap[element]++;
+        } else {
+            frequencyMap[element] = 1;
+        }
+    });
+
+    // Find the most frequent element
+    let mostFrequentElement;
+    let maxFrequency = 0;
+    for (const element in frequencyMap) {
+        if (frequencyMap[element] > maxFrequency) {
+            maxFrequency = frequencyMap[element];
+            mostFrequentElement = element;
+        }
+    }
+
+    return parseInt(mostFrequentElement);
+}
+
+// const array = [3, 5, 2, 5, 3, 3, 1, 4, 5];
+// console.log(findMostFrequentElement(array)); 
+
